82488762574597c1c9026ea75816633a3f24c23c
m-sokolskiy <rognarius@icloud.com>
2024-09-06 14:19:22 +0300

    Start React App

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-
-const todos = {
-  todos: [
-    {
-      text: "посмотреть новый урок",
-      id: 15,
-      created_at: "2023-07-05T15:14:15.080Z",
-      user: null,
-    },
-    {
-      text: "сдать домашку",
-      id: 16,
-      created_at: "2023-07-05T15:14:42.499Z",
-      user: null,
-    },
-    {
-      text: "поплакать",
-      id: 17,
-      created_at: "2023-07-05T15:14:42.499Z",
-      user: null,
-    },
-  ]
-}
-
 const App = () => { 
 
-  const [value, setValue] = useState('');
-
-  const addTodo = () => {
-    alert('clicked')
-  };
-
+  
   return (
     <div>
-      <ul>
-        {todos.todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
-        <input value={value} onChange={(event) => {
-          setValue(event.target.value)
-        }} />
-        <button onClick={addTodo}>Добавить</button>
+      <h1>Привет</h1>
     </div>
   );
 }
