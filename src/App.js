9faea7a57cf5e028c54bd9790b708d9694b5af9a
m-sokolskiy <rognarius@icloud.com>
2025-04-15 23:21:32 +0300

    15.04.2025

--- src/App.js
+++ src/App.js
@@ -2,47 +2,42 @@ import { useState } from "react";
 
 const App = () => {
 
-  const orders = [
-    { id: 1, price: "100$", status: "done" },
-    { id: 2, price: "250$", status: "pending" },
-    { id: 3, price: "400$", status: "done" },
-    { id: 4, price: "150$", status: "cancelled" },
-    { id: 5, price: "300$", status: "done" },
+  const products = [
+    { id: 1, name: "Ноутбук", price: 800 },
+    { id: 2, name: "Телефон", price: 500 },
+    { id: 3, name: "Клавиатура", price: 100 },
+    { id: 4, name: "Мышь", price: 50 }
   ];
 
-  const [order, setOrder] = useState(orders);
-  const [prices, setPrices] = useState(false);
-  const [sum, setSum] = useState(0);
+  const [productsList] = useState(products)
+  const [cart, setCart] = useState([])
 
-  const filterDone = () => {
-    const filteredDone = order.filter((item) => item.status === "done");
-    setOrder(filteredDone);
-  };
-
-  const sumPrice = () => {
-    const result = order.map(item => parseInt(item.price)).reduce((acc, item) => acc + item)
-    console.log(result);
-    setSum(result)
-  };
-
-  const filterPrice = () => {
-    setPrices(true)
+  const addToCart = (item) => {
+    setCart([...cart, item])
   }
 
   return (
     <div>
-      {sum ? (<p>Сумма: {sum}</p>) : (<ul>
-        {order.map((item => (
-          <li key={item.id}>{prices ? `Цена: ${item.price}` : `Товар: ${item.id} Цена: ${item.price} Статус: ${item.status}`}</li>
-        )))}
-      </ul>)}
-
-      <button onClick={filterDone}>Вывести только завершённые заказы</button>
-      <button onClick={filterPrice}>Из них — получить только цены.</button>
-      <button onClick={sumPrice}>Посчитать сумму всех завершённых заказов.</button>
-
+      <h3>Список Товаров</h3>
+      <ul>
+        {productsList.map(item => {
+          return (
+            <li key={item.id}>{`${item.name}, цена: ${item.price}`}
+              <button onClick={() => addToCart(item)}>Добавить в корзину</button></li>
+          )
+        })}
+      </ul>
+      <h3>Корзина</h3>
+      <ul>
+        {cart.map(item => {
+          return (
+            <li key={item.id}>{`${item.name}, цена: ${item.price}`}
+              <button>Удалить из корзины</button></li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 
 }
 
