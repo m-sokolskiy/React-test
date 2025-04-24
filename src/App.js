e35bd6051336245c7422e767e1f9daa68fe9f54d
m-sokolskiy <rognarius@icloud.com>
2025-04-24 22:29:57 +0300

    24.04.25

--- src/App.js
+++ src/App.js
@@ -2,66 +2,37 @@ import { useState } from "react";
 
 const App = () => {
 
-  const products = [
-    { id: 1, name: "Ноутбук", price: 800 },
-    { id: 2, name: "Телефон", price: 500 },
-    { id: 3, name: "Клавиатура", price: 100 },
-    { id: 4, name: "Мышь", price: 50 }
-  ];
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState("")
 
-  const [productsList] = useState(products)
-  const [cart, setCart] = useState([])
-  const [sum, setSum] = useState()
+  const addTask = (inputValue) => {
 
-  const addToCart = (item) => {
-    if (!cart.some(cartItem => cartItem.id === item.id)) {
-      setCart([...cart, item])
+    const newTask = {
+      id: Date.now(),
+      message: inputValue,
+      completed: false,
     }
+    setTasks([...tasks, newTask])
+    setInputValue()
+    console.log(tasks);
   }
 
-  const removeFromCart = (id) => {
-    setCart(cart.filter(cartItem => cartItem.id !== id));
-  };
-
-  const removeCart = () => {
-    setCart([])
-    setSum()
-  };
-
-  const sumCart = () => {
-    const result = cart.reduce((acc, item) => {
-    return acc + item.price  
-    }, 0)
-    setSum(result)
-  };
+  // const completedTask = (item) => {
+  //   setCompletedValue(!item.completed)
+  // }
 
   return (
     <div>
-      <h3>Список Товаров</h3>
+      <h1>Список задач</h1>
       <ul>
-        {productsList.map(item => {
+        {tasks.map((item) => {
           return (
-            <li key={item.id}>{`${item.name}, цена: ${item.price}`}
-              <button onClick={() => addToCart(item)}>Добавить</button></li>
+            <li key={item.id}>{item.message}<button>Выполнено</button></li>
           )
         })}
       </ul>
-      <h3>Корзина</h3>
-      <ul>
-        {cart.map(item => {
-          return (
-            <li key={item.id}>{`${item.name}, цена: ${item.price}`}
-              <button onClick={() => removeFromCart(item.id)}>Удалить</button></li>
-          )
-        })}
-      </ul>
-      <div>
-      <button onClick={sumCart}>Посчитать сумму</button>
-      {sum ? <h3>Сумма: {sum} </h3> : <h3>Сумма:</h3>}
-      </div>
-
-
-      <button onClick={removeCart}>Очистить корзину</button>
+      <input placeholder="Добавь задачу" onChange={(event) => setInputValue(event.target.value)}></input>
+      <button onClick={() => addTask(inputValue)}>Добавить задачу</button>
     </div>
   )
 
