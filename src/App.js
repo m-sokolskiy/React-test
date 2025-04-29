5b3c2be14ad91a279c72fc37552c7db2149e3be7
m-sokolskiy <rognarius@icloud.com>
2025-04-29 22:08:33 +0300

    29.04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
 
@@ -12,26 +12,42 @@ const App = () => {
       message: inputValue,
       completed: false,
     }
-    setTasks([...tasks, newTask])
-    setInputValue()
-    console.log(tasks);
+
+    if (inputValue.length <= 0) {
+      alert("Введите задачу")
+    } else {
+      setTasks([...tasks, newTask])
+      setInputValue("")
+    }
   }
 
-  // const completedTask = (item) => {
-  //   setCompletedValue(!item.completed)
-  // }
+  useEffect(() => {
+    console.log(inputValue);
+  }, [])
+
+  const completedTask = (data) => {
+    return !data
+  }
+
+  const completedTasks = () => {
+    return setTasks(tasks.filter((el) => el.completed === true)) 
+  }
+
 
   return (
     <div>
       <h1>Список задач</h1>
+      <button>Все</button>
+      <button>Активные</button>
+      <button onClick={() => completedTasks()}>Выполненные</button>
       <ul>
         {tasks.map((item) => {
           return (
-            <li key={item.id}>{item.message}<button>Выполнено</button></li>
+            <li key={item.id}>{item.message}<button onClick={() => completedTask(item.completed)}>Выполнено</button><button>Удалить</button></li>
           )
         })}
       </ul>
-      <input placeholder="Добавь задачу" onChange={(event) => setInputValue(event.target.value)}></input>
+      <input value={inputValue} placeholder="Добавь задачу" onChange={(event) => setInputValue(event.target.value)}></input>
       <button onClick={() => addTask(inputValue)}>Добавить задачу</button>
     </div>
   )
